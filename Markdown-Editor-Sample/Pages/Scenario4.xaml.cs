022383cdc5fa6410b21a1b864620303d8c176f6c
安然 张 <zar234@qq.com>
2020-09-06 10:51:35 +0800

    update

--- Markdown-Editor-Sample/Pages/Scenario4.xaml.cs
+++ Markdown-Editor-Sample/Pages/Scenario4.xaml.cs
@@ -1,31 +1,17 @@
 ﻿using MarkdownEditBox.Enums;
 using MarkdownEditBox.Models;
-using Newtonsoft.Json;
 using Richasy.Controls.UWP.Models.UI;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
 namespace Markdown_Editor_Sample.Pages
 {
     /// <summary>
-    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// This scene is suitable for editor theme changes
     /// </summary>
     public sealed partial class Scenario4 : RichasyPage
     {
